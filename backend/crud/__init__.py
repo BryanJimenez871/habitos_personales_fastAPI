970260c1876225ed820b03ff548fdb299c131b43
Bryan <jimenezbryan871@gmail.com>
2026-03-14 12:17:27 -0300

    Corección de archivos duplicados

--- backend/crud/__init__.py
+++
@@ -1,9 +0,0 @@
-from .habit import HabitCrud
-from .date import DateCrud
-from .daily_habits import DailyHabitsCrud
-
-__all__ = [
-    "HabitCrud",
-    "DateCrud",
-    "DailyHabitsCrud",
-]
--- backend/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .habit import router as habit_router
-from .daily import router as daily_router
-from .date import router as date_router
-
-__all__ = [
-    "habit_router",
-    "daily_router",
-    "date_router"
-]
